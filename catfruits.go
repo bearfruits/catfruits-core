91cd0dbca6dad297b9737a81c056895a637c4002
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-12-23 15:44:40 +0900

    Split files

--- catfruits.go
+++ catfruits.go
@@ -1,11 +1,5 @@
 package catfruits
 
-import (
-	"io/ioutil"
-	"os"
-	"path/filepath"
-)
-
 // Infomation is a repository infomation.
 type Infomation struct {
 	Flameworks []string `json:"flameworks"`
@@ -47,23 +41,4 @@ func (sc *Scanner) Scan() (*Infomation, error) {
 	}
 
 	return info, nil
-}
-
-// ----------- Helper functions
-
-// FilePath returns a filepath
-func (sc *Scanner) FilePath(path string) string {
-	return filepath.Join(sc.dir, path)
-}
-
-// FileExist returns a bool
-func (sc *Scanner) FileExist(path string) bool {
-	fname := sc.FilePath(path)
-	_, err := os.Stat(fname)
-	return err == nil
-}
-
-// ReadFile returns a file value
-func (sc *Scanner) ReadFile(path string) ([]byte, error) {
-	return ioutil.ReadFile(sc.FilePath(path))
 }
