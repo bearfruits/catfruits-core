c4b4b989ec3127e9e8560f98813fe0fa2a589182
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-01-31 14:43:13 +0900

    JavaScript frameworks

--- framework.go
+++ framework.go
@@ -2,13 +2,28 @@ package catfruits
 
 // DefaultFrameworkFuncs is default framework set
 var DefaultFrameworkFuncs = map[string]FrameworkFunc{
-	"Ruby on Rails": FWRails,
+	// Ruby
+	"Ruby on Rails": fwRails,
+
+	// JavaScript
+	"Vue":      fwJSGen("vue"),
+	"React":    fwJSGen("react"),
+	"Angular":  fwJSGen("angular"),
+	"backbone": fwJSGen("backbone"),
 }
 
 // FWRails detects Ruby on Rails
-func FWRails(sc *Scanner) (bool, error) {
+func fwRails(sc *Scanner) (bool, error) {
 
 	return sc.hasPackage("gem", "rails") ||
 		sc.FileExist("script/rails") ||
 		sc.FileExist("bin/rails"), nil
+}
+
+// bower と npm から探索する関数をかえす
+func fwJSGen(name string) FrameworkFunc {
+	return func(sc *Scanner) (bool, error) {
+		return sc.hasPackage("bower", name) ||
+			sc.hasPackage("npm", name), nil
+	}
 }
